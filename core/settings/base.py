5a3450ff41635c331bf2bd9d07ec4f0ac4dc735b
Anil Kumar <anil.bhutale@hedgehog.com>
2023-08-12 22:37:26 +0000

    jwt added

--- core/settings/base.py
+++ core/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'frontend',
     'backend',
     'api',
+    'aoi.user'
     'cloudinary'
 ]
 
--- core/settings/prod.py
+++ core/settings/prod.py
@@ -47,8 +47,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
